d78bdeb9290fd82310c8a275a8f31cfffbb284e9
Małgorzata Kurach <96025745+mkurach@users.noreply.github.com>
2022-11-29 17:37:51 +0100

    counting elapsed time on threads

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -3,34 +3,46 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <chrono>
+#include <ctime>
+#include <vector>
 
 
 int main(int argc, char *argv[]) {
 
 
-    /*if(argc != 2) {
-        std::cout<<"Zla ilosc plikow, podaj 2 macierze"<<std::endl;
+    if(argc != 4) {
+        std::cout<<"Zla ilosc plikow, podaj 2 macierze i plik wyjsciowy"<<std::endl;
         exit(1);
     } 
-    else {*/
+    else {
         std::ifstream file1(argv[1]);
         std::ifstream file2(argv[2]);
+        std::ofstream fileout(argv[3]);
+        if (!file1 || !file2 || !fileout) {
+            std::cout<<"Nie da się otworzyć! :("<<std::endl;
+            exit(1);
+        }
         Macierz mac1(file1);
         Macierz mac2(file2);
-        std::unique_ptr<Macierz> wynik = mnozenie_macierzy (mac1, mac2, 2);
-        std::cout<<*wynik;
-        //std::cout<<mac1;
-    //}
+
+        using namespace std::chrono;
+        int threads = 6;
+        std::vector< time_point<system_clock> > start, end;
+        std::vector<duration<double> > elapsed_seconds;
+        std::unique_ptr<Macierz> wynik;
+        for(int i = 2; i <= threads; i++ ) {
+            start.push_back(system_clock::now());
+            wynik = mnozenie_macierzy (mac1, mac2, i);
+            end.push_back(system_clock::now());
+            elapsed_seconds.push_back(end.back()-start.back());
+            std::cout<<"Ile czasu uplynelo na "<<i<<" watkach: "<<elapsed_seconds.back().count()<<"s\n";
+
+        }
+
+        fileout<<*wynik;
+    }
 
 
     return 0;
 }
-/*const clockid_t ZegarID = CLOCK_REALTIME;
-timespec start, koniec; // Struktury przechowujące czas
-std::cout << "Mnozenie macierzy...";
-std::cout.flush ();
-clock_gettime (ZegarID, &start); // Zapisuje czas początku mnożenia
-// Mnoży macierze a i b (liczba oznacza liczbę równoległych wątków)
-std::unique_ptr<Macierz> wynik = mnozenie_macierzy (a, b, 2);
-clock_gettime (ZegarID, &koniec); // Zapisuje czas końca mnożenia
-std::cout << " zajelo " << interwal (start, koniec) << " s" << std::endl;*/
